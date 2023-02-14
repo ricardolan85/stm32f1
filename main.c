353a8bad252cb5010740c22d84848ad7ea83363f
Ricardo Landim <ricardolan@gmail.com>
2023-02-14 18:50:02 -0300

    alterando para pb3

--- main.c
+++ main.c
@@ -2,17 +2,23 @@
 
 void setup(){
 
-  RCC->APB2ENR |= (1 << 4); //habilitado clock para IOPC
-  GPIOC->CRH |= (2 << 20); //MODE13 = 10 -  0x2 (output mode, max 2 mhz)
-  GPIOC->CRH &= ~(3 << 22); //CNF13 = 00 - ~0x3 (output push-pull)
- 
+  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN; //habilitado clock para PB
+
+  //PB3
+  GPIOB->CRL &= ~(3 << 12); //MODE3 = 00 -  0x0 clear mode
+  GPIOB->CRL |= (2 << 12); //MODE3 = 10 -  0x2 (output mode, max 2 mhz)
+  GPIOB->CRL &= ~(3 << 14); //CNF3 = 00 - ~0x3 (output push-pull)
+
 }
 
 void loop(){
 
-  GPIOC->BSRR |= (1 << 13); //set PC13
+  //on
+  GPIOB->BSRR |= (1 << 3); //set PB3
   for (uint32_t i = 0; i < 1000000; i++); //delay
-  GPIOC->BSRR |= (1 << 29); //reset PC13
+  
+  //off
+  GPIOB->BSRR |= (1 << 19); //reset PB3
   for (uint32_t i = 0; i < 1000000; i++); //delay
   
 }
