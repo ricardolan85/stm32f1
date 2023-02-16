85735ac1d895326c67d57628d2daaffed86d3052
Ricardo Landim <ricardolan@gmail.com>
2023-02-16 18:20:17 -0300

    alterando o tamanho do vetor de interrupções para 68

--- ivt.c
+++ ivt.c
@@ -1,7 +1,7 @@
-#define CORTEXM_IVT_SIZE 16
-#define STM32F1_IVT_SIZE 43
+#define CORTEXM3_IVT_SIZE 16
+#define STM32F1X_IVT_SIZE 68
 
 extern void _reset(void);
 extern void _estack(void);
 
-__attribute__((section(".vectors"))) void (*tab[CORTEXM_IVT_SIZE + STM32F1_IVT_SIZE])(void) = {_estack, _reset};
+__attribute__((section(".vectors"))) void (*tab[CORTEXM3_IVT_SIZE + STM32F1X_IVT_SIZE])(void) = {_estack, _reset};
